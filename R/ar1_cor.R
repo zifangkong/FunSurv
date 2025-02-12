6db7cb74a371dcc37d16a5f5f747de6ea64412a8
Steven Chiou <syhan.chiou@gmail.com>
2025-02-12 08:39:02 -0600

    swap out ar1_cor

--- R/ar1_cor.R
+++ R/ar1_cor.R
@@ -1,4 +1,4 @@
-#' Function to construct AR(1) correlation matrix
+#' Construct an AR(1) correlation matrix
 #'
 #' @param n number of events for each subject
 #' @param rho autoregressive correlation
@@ -7,6 +7,14 @@
 #' 
 #' @export
 ar1_cor <- function(n, rho) {
-  exponent <- abs(matrix(1:n - 1, nrow = n, ncol = n, byrow = TRUE) - (1:n - 1))
-  return(rho^exponent / (1 - rho^2))
+  d <- c(n, n)
+  rho^abs(.col(d) - .row(d)) / (1 - rho^2)
 }
+
+## old version from https://www.r-bloggers.com/2020/02/generating-correlation-matrix-for-ar1-mode/l
+## why? you don't even need the "-1" at these 2 places in exponent
+## ar1_cor <- function(n, rho) {
+##   exponent <- abs(matrix(1:n - 1, nrow = n, ncol = n, byrow = TRUE) - (1:n - 1))
+##   rho^exponent / (1 - rho^2)
+## }
+
