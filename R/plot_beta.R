456f19cc06677df3da53759d388356acc6ea6183
zifangkong <zifangkong@gmail.com>
2025-02-24 00:24:23 -0600

    Update plot_beta.R

--- R/plot_beta.R
+++ R/plot_beta.R
@@ -6,6 +6,6 @@
 #' @export
 plot_beta <- function(object){
    npc <- object$PACE$npc
-   beta <- object$ebeta[-(1:(nrow(object$ebeta)-npc)), 1] %*% object$PACE$functions@X
+   beta <- crossprod(object$PACE$functions@X, object$ebeta[-(1:(nrow(object$ebeta) - npc)), 1])
    plot(object$PACE$functions@argvals[[1]], beta, type="l", xlab="Follow-up time", ylab=expression("Estimate of " * beta(t)))
 }
