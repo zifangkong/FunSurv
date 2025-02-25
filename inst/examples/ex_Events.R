9eeee06d5f4baa2493167feefded3bba7a5727bd
zifangkong <zifangkong@gmail.com>
2025-02-25 12:24:05 -0600

    Update ex_Events.R

--- inst/examples/ex_Events.R
+++ inst/examples/ex_Events.R
@@ -1,1 +1,6 @@
+## ex1
 Events(1:5, 2:6, id=1:5, status=c(1,1,0,1,0)) 
+
+## ex2
+load("../../data/surv_data.RData")
+Events(surv_data$t_start, surv_data$t_stop, id=surv_data$id, status=surv_data$id)
