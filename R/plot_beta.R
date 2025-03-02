2425c8af6cc6bd78c1a381d77a90504182f9fc77
zifangkong <zifangkong@gmail.com>
2025-03-02 10:13:25 -0800

    Update plot_beta.R

--- R/plot_beta.R
+++ R/plot_beta.R
@@ -1,17 +1,3 @@
-#' Plot functional coefficient
-#'
-#' @param object An AR1_FRAILTY object
-#'
-#' @returns a plot of functional coefficient
-#' @export
-#' @example inst/examples/ex_plot_beta.R
-plot_beta <- function(object){
-  npc <- object$PACE$npc
-  beta <- crossprod(object$PACE$functions@X, object$ebeta[-(1:(nrow(object$ebeta) - npc)), 1])
-  plot(object$PACE$functions@argvals[[1]], beta, type="l", xlab="Follow-up time",
-       ylab=expression("Estimate of " * beta(t)))
-}
-
 #' Plot method for 'funsurv' objects
 #'
 #' Plots beta(t) ...
@@ -24,7 +10,7 @@ plot_beta <- function(object){
 plot.funsurv <- function(x) {
   if (!is.funsurv(x)) stop("Must be a funsurv object")
   npc <- x$PACE$npc
-  beta <- crossprod(x$PACE$functions@X, x$ebeta[-(1:(nrow(x$ebeta) - npc)), 1])
+  beta <- crossprod(x$PACE$functions@X, x$beta[-(1:(length(x$beta) - npc))])
   ggplot(NULL, aes(x = x$PACE$functions@argvals[[1]], y = beta)) +
     geom_line() + xlab("Follow-up time") + ylab(expression("Estimate of " * beta(t)))
 }
