6e6998cbb1089a2bbe18a77e384a9a86a85632e0
zifangkong <zifangkong@gmail.com>
2025-03-02 10:12:04 -0800

    Update data.R

--- R/data.R
+++ R/data.R
@@ -7,10 +7,18 @@
 #' @description Survival data \code{sdat}
 #' \describe{
 #'   \item{id}{subjects identification}
+#'   \item{event}{sequence of the number of events per subject}
+#'   \item{t_start}{event starting time}
+#'   \item{t_end}{event end time}
+#'   \item{censoring_time}{event censoring time}
+#'   \item{status}{event status: status=1 if event observed and status=0 if event censored}
+#'   \item{z1}{a univariate scalar covariates. Can be extended to multiple scalar covariates}
 #' }
 #' @description Functional data \code{fdat}
 #' \describe{
 #'   \item{id}{subjects identification}
+#'   \item{time}{time points for each longitudinal measurement}
+#'   \item{x}{longitudinal measurements at distinct time points}
 #' }
 #'
 #' @usage data(simDat)
