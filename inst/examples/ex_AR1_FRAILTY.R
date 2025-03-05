290ab697e9d5758a5e0ae423c3c2ca10b86cac75
Zifang Kong <zifangkong@gmail.com>
2025-03-05 01:28:25 -0600

    Update ex_AR1_FRAILTY.R

--- inst/examples/ex_AR1_FRAILTY.R
+++ inst/examples/ex_AR1_FRAILTY.R
@@ -1,15 +1,6 @@
-data(simDat)
-
-## Calculate event-specific FPC scores
-## 90% leads to 3 FPC components
-fpc_obj <- AR1_PACE(fdat, sdat, nbasis = 10, pve = 0.9) 
-
-## Fit the functional regression with autoregressive frailty 
-fit <- AR1_FRAILTY(formula = Events(t_start, t_stop, id, status) ~ z1, data = sdat, fpca_obj = fpc_obj, para0 = c(0.5, 0.5))
-
+fit <- AR1_FRAILTY(formula=Recur(t_start %to% t_stop, id, event=status) ~ z1, sdat=sdat, fdat=fdat)
 
 str(basesurv(fit))
-
-fit
+print(fit)
 summary(fit)
 plot(fit)
