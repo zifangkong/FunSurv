05ac1106686fe48ae1652ab80359b700278a6ec5
Steven Chiou <syhan.chiou@gmail.com>
2025-03-09 23:03:14 -0500

    delete extra files

--- R/basesurv.R
+++ R/basesurv.R
@@ -5,7 +5,6 @@
 #' @returns A data frame including time and baseline survival
 #' @export
 #' @example inst/examples/ex_basesurv.R
-
 basesurv <- function(object){
   if (!is.funsurv(object)) stop("Must be a funsurv object")
    data.frame(time = object$time, basesurv = object$basesurv)
