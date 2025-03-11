f4b32e07a8e89df74ed6956454c12c58bd351213
Zifang Kong <zifangkong@gmail.com>
2025-03-11 16:14:21 -0500

    Update ex_plot.R

--- inst/examples/ex_plot.R
+++ inst/examples/ex_plot.R
@@ -1,6 +1,6 @@
 data(simDat)
 fit <- AR1_FRAILTY(Recur(t_start %to% t_stop, id, status) ~ z1,
-                   sdat = sdat, fdat = fdat)
+                   sdat = sdat, fdat = fdat, iter.max = 30)
 plot(fit, what = "beta")
 plot(fit, what = "fpc")
 plot(fit, what = "basesurv")
