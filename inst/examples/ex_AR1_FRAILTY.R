d2f002694062036f45daadc5d74a3456105b557f
Zifang Kong <zifangkong@gmail.com>
2025-03-11 16:14:16 -0500

    Update ex_AR1_FRAILTY.R

--- inst/examples/ex_AR1_FRAILTY.R
+++ inst/examples/ex_AR1_FRAILTY.R
@@ -1,6 +1,6 @@
 data(simDat)
 
 fit <- AR1_FRAILTY(Recur(t_start %to% t_stop, id, status) ~ z1,
-                   sdat = sdat, fdat = fdat)
+                   sdat = sdat, fdat = fdat, iter.max = 30)
 
 summary(fit)
